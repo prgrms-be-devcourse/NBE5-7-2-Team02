66ea4d0525bf14ccd0b05573bec441ca3ac690c2
STGRAM\gffd9 <29088981l@gmail.com>
2025-05-12 09:29:55 +0900

    fix PR comment 수정

--- NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/member/dao/FollowRepository.java
+++ NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/member/dao/FollowRepository.java
@@ -11,10 +11,10 @@ import org.springframework.data.repository.query.Param;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
-    @Query("SELECT f.follower FROM Follow f where f.follower = :member")
+    @Query("SELECT f.follower FROM Follow f where f.following = :member")
     List<Member> findFollowerMembers(@Param("member") Member member);
 
-    @Query("select f.following from Follow f where f.following = :member")
+    @Query("select f.following from Follow f where f.follower = :member")
     List<Member> findFollowingMembers(@Param("member") Member member);
 
     boolean existsByFollowerAndFollowing(Member follower, Member following);
