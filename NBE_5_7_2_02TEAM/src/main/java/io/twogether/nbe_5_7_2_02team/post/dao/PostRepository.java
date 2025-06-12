70838c5d4b87dd4dcd3c9cedb389f284694f59ca
Jeongmin39 <wjdals20309@naver.com>
2025-06-12 12:08:15 +0900

    feat: Repository 생성 및 메서드 추가

--- NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/post/dao/PostRepository.java
+++ NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/post/dao/PostRepository.java
@@ -2,6 +2,8 @@ package io.twogether.nbe_5_7_2_02team.post.dao;
 
 import io.twogether.nbe_5_7_2_02team.post.domain.Post;
 
+import io.twogether.nbe_5_7_2_02team.post.domain.RecruitmentStatus;
+import java.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +14,8 @@ public interface PostRepository extends JpaRepository<Post, Long>, PostRepositor
 
     @Query("SELECT p FROM Post p WHERE p.member.id = :memberId")
     List<Post> findAllByMemberId(@Param("memberId") Long memberId);
+
+    List<Post> findByRecruitmentStatusAndRecruitmentDeadlineBefore(
+        RecruitmentStatus status, LocalDate deadline
+    );
 }
