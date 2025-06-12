80dc703eb65830f0048b48c8d560eb87bb2e9ae3
Kim, Seokwan <sukwan1106@gmail.com>
2025-06-12 12:43:19 +0900

    test: OAuth 테스트 코드

--- NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/oauth/dao/RefreshTokenBlackListRepository.java
+++ NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/oauth/dao/RefreshTokenBlackListRepository.java
@@ -3,9 +3,12 @@ package io.twogether.nbe_5_7_2_02team.oauth.dao;
 import io.twogether.nbe_5_7_2_02team.oauth.domain.RefreshToken;
 import io.twogether.nbe_5_7_2_02team.oauth.domain.RefreshTokenBlackList;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RefreshTokenBlackListRepository
         extends JpaRepository<RefreshTokenBlackList, Long> {
     void deleteByRefreshToken(RefreshToken refreshToken);
+
+    Optional<RefreshTokenBlackList> findByRefreshTokenId(Long refreshTokenId);
 }
