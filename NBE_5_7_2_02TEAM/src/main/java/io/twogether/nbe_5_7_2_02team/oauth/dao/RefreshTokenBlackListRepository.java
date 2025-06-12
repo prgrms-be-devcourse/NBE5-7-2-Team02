8846f77598df21372a4b739ce8c37882c136e48f
ksw733 <129919107+ksw733@users.noreply.github.com>
2025-06-12 15:04:37 +0900

    test: OAuth 테스트 코드 (#136)
    * test: OAuth 테스트 코드
    
    * chore: Java 스타일 수정
    
    * fix: package 파일 삭제
    
    * test: OAuth 테스트 코드
    
    ---------
    
    Co-authored-by: github-actions <>

--- NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/oauth/dao/RefreshTokenBlackListRepository.java
+++ NBE_5_7_2_02TEAM/src/main/java/io/twogether/nbe_5_7_2_02team/oauth/dao/RefreshTokenBlackListRepository.java
@@ -5,7 +5,11 @@ import io.twogether.nbe_5_7_2_02team.oauth.domain.RefreshTokenBlackList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenBlackListRepository
         extends JpaRepository<RefreshTokenBlackList, Long> {
     void deleteByRefreshToken(RefreshToken refreshToken);
+
+    Optional<RefreshTokenBlackList> findByRefreshTokenId(Long refreshTokenId);
 }
